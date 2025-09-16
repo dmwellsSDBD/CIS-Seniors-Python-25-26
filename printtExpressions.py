b83e598f847725d36498368d6a57e969d3e14a08
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-09-16 13:53:08 +0000

    Tuesday 9/16

---
+++ printtExpressions.py
@@ -0,0 +1,16 @@
+'''
+Class: CIS Seniors 25-26
+Name: David Wells
+Assignment: Printing Expressions
+'''
+
+print("Hello, my name is David.")
+
+print("My favorite song!")
+print("-----------------")
+print()
+print("...the first 4 lines of your song.")
+print("Hey Jude, don't make it bad,")
+print("Take a sad song, and make it better")
+print("Remember, to let her into your heart")
+print("Then you can start to make it better")
