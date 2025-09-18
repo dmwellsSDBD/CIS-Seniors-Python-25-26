2f45db850e8bd68b5d88f2cb462c55b0f9714146
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-09-18 13:27:51 +0000

    Thursday part 1

---
+++ morePyVars.py
@@ -0,0 +1,35 @@
+''' 
+The Doc String
+Description: Notes about Python Variables
+Name: David Wells
+Date: 9/18/25
+'''
+print("\n\n\n")
+# Variable Assignment
+firstName = "David"
+lastName = "Wells"
+gradeLevel = 45
+favoriteSubject = "Compter Science"
+yearsInSchool = "too many"
+
+print("My name is " + firstName + " " + lastName + ".")
+print("I am in" , gradeLevel , "th grade.")
+print("My favorite subject is " + favoriteSubject)
+print("I have been in school for " + yearsInSchool)
+
+print("\n\n\n")
+
+print("Mathmatical Calculations:")
+
+# Storing mathematical values
+length = 15
+width = 8
+height = 12
+
+# Calculate area and volume and store them in variables
+area = length * width
+volume = length * width * height
+
+# print out the values
+print("The area of our sturcture is: ", area)
+print("The volume of our structure is: " , volume)
