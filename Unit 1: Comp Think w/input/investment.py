8ed3383a1313430d057e625124c362b308548e40
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-09-22 14:21:13 +0000

    Monday's Class changes

---
+++ Unit 1: Comp Think w/input/investment.py
@@ -0,0 +1,37 @@
+"""
+Program: investment.py
+Author: David Wells
+Class: CIS Seniors
+Date: 9/22/25
+
+Compute an Investment Report
+1. The inputs are:
+    starting investment amount
+    number of years
+    Interest rate (an Integer percent)
+
+2. The report is displayed in tabular form with a header
+3. Computations and outputs:
+    for each year of the investment
+        compute the interest and add it to the investment
+        print a formatted row of results for that year
+4. The ending invesment and interest you have paid in total are also displayed
+"""
+print("\n\nMy Investment Calculator")
+print("=" *25)
+# Accept the inputs
+
+
+# Convert the interest rate to a decimal number
+
+
+# Initialize the accumulator for the interest
+
+
+# display the header for the table
+
+
+# Compute and display the results for each year
+
+
+# Display the totals for the period
---
+++ Unit 1: Comp Think w/input/taxForm.py
@@ -0,0 +1,42 @@
+"""
+Program: taxForm.py
+Author: David Wells
+Class: CIS Seniors
+Date: 9/22/25
+
+Compute a person's income tax.
+1. Significant constants
+    tax rate
+    standard deduction
+    deduction per dependent
+
+2. The inputs are
+    gross income
+    number of dependents
+
+3. Computations:
+    taxable income = gross income - the standard deduction - a deduction for each dependent
+    income tax = is a fixed percentage of the taxable income
+
+4. The outputs are:
+    the income tax
+"""
+print("\n\nMy Tax Rate Calculator.")
+print("=" * 40)
+
+# Initialize the constants
+TAX_RATE = 0.20
+STANDARD_DEDUCTION = 10000.0
+DEPENDENT_DEDUCTION = 3000.0
+
+# Request the inputs
+grossIncome = float(input("Enter the gross income: "))
+numDependents = int(input("Enter the number of dependents: "))
+
+# Compute the income tax
+taxableIncome = grossIncome - STANDARD_DEDUCTION - \
+                DEPENDENT_DEDUCTION * numDependents
+incomeTax = taxableIncome * TAX_RATE
+
+# Display the income tax
+print("The income tax is $" + str(incomeTax) + "\n\n")
