0094d25070599008747d45744e116cf4b8aec535
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-09-23 14:52:00 +0000

    Tuesday's Work

---
+++ Unit 1: Comp Think w/investmentBook.py
@@ -0,0 +1,34 @@
+"""
+Program: investmentBook.py
+Author: David
+Compute an investment report.
+
+1. The inputs are...
+"""
+print("\n\n")
+print("=" * 100)
+print("My Investment Tracker")
+print("=" * 50)
+
+# Accept the Inputs
+startBalance = float(input("Enter the investment amount: "))
+years = int(input("Enter the number of years: "))
+rate = int(input("Enter the rate as a %: "))
+
+# Convert the rate to a decimal number
+rate = rate / 100
+
+# Initiialize the accumulator for the interest
+totalInterest = 0.0
+
+# Display the header for the table
+
+
+
+
+# Compute and display the results for each year
+
+
+
+
+# Display the totals for the period
