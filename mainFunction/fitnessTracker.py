3b0e40b28332613d3422482ef7e37b40a0498b9c
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-11-18 13:05:12 +0000

    Tuesday fitness tracker setup

---
+++ mainFunction/fitnessTracker.py
@@ -0,0 +1,44 @@
+def get_subject():
+    # TODO: Ask user for subject
+    pass
+
+def get_duration():
+    # TODO: Get study time in minutes
+    pass
+
+def calculate_break(study_time):
+    # TODO: Calculate 10% for break
+    pass
+
+def display_summary(subject, study_time, break_time):
+    # TODO: Show the study plan
+    pass
+
+def main():
+    # TODO: Coordinate all functions
+    pass
+
+if __name__ == "__main__":
+    main()
+
+"""
+# UNSTRUCTURED PROGRAM - Needs fixing!
+print("Fitness Tracker")
+print("===============")
+
+exercise = input("What exercise did you do? ")
+duration_str = input("How many minutes? ")
+duration = int(duration_str)
+
+calories_per_minute = 8  # Average
+total_calories = duration * calories_per_minute
+
+print("Exercise: " + exercise)
+print("Duration: " + str(duration) + " minutes")
+print("Calories burned: " + str(total_calories))
+
+if total_calories >= 300:
+    print("Great workout!")
+else:
+    print("Good start! Try for 30+ minutes next time.")
+"""
